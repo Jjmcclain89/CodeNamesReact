52beaa5031cadd5269b664f51009c1a59f054ee2
Josh McClain <jjmcclai@uiowa.edu>
2019-03-22 11:03:52 -0500

    .

---
+++ frontend/src/components/lobby.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class lobby extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return <div />;
+    }
+}
+
+export default lobby;
